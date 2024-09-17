7f1750703aff16a7e3edc6c52f7bf629a2b5aadb
Tim1527 <86481222+Tim1527@users.noreply.github.com>
2024-09-17 18:16:06 +0300

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -6,6 +6,28 @@ user_id = 0
 HotkeyMode = False
 ScrollMode = False
 
+KEYBOARD_KEYS = str(['\t', '\n', '\r', ' ', '!', '"', '#', '$', '%', '&', "'", '(',
+')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7',
+'8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
+'a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~',
+'accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace',
+'browserback', 'browserfavorites', 'browserforward', 'browserhome',
+'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear',
+'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete',
+'divide', 'down', 'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10',
+'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20',
+'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9',
+'final', 'fn', 'hanguel', 'hangul', 'hanja', 'help', 'home', 'insert', 'junja',
+'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail',
+'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack',
+'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6',
+'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn',
+'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn',
+'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator',
+'shift', 'shiftleft', 'shiftright', 'sleep', 'space', 'stop', 'subtract', 'tab',
+'up', 'volumedown', 'volumemute', 'volumeup', 'win', 'winleft', 'winright', 'yen',
+'command', 'option', 'optionleft', 'optionright'])
 ru,eng = [],[]
 for x in "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя":
     ru.append(x)
@@ -48,7 +70,8 @@ def password_enter(message):
         connection = True
         TL = Telega.Telega(message)
         id = message.from_user.id
-        TL.Reply_btns("Connection established",True,4, "/LKM","/2LKM","/Enter","/Bcksp","/RKM","/Scroll","/Hotkey","/Shift","/PrtSc", "/Cam","/session")
+        TL.Reply_btns("Connection established",True,4, "/LKM","/2LKM","/Enter","/Bcksp",
+                      "/RKM","/Scroll","/Hotkey","/Shift","/PrtSc", "/Cam","/session")
         bot.delete_message(message.chat.id, message.id)
     else:
         bot.send_message(message.chat.id,"Wrong password, try again.")
@@ -114,9 +137,12 @@ def RKM(message):
 
 @bot.message_handler(commands = ["Hotkey"])
 def Hotkey(message):
+    TL = Telega.Telega(message)
+
     global HotkeyMode
     if connection and message.from_user.id == id:
-        bot.send_message(message.chat.id,"Enter hotkey")
+        TL.Reply_btns("Enter hotkey", True, 3, "Esc", "Alt+Shift","Alt+Tab","Win+r",
+                      "Ctrl+Shift+Esc","ctrl+alt+del","Show_keyboard_keys") #По каким-то волшебным причинам Ctrl+Alt+Delete не равботает)
         HotkeyMode = True
 
 @bot.message_handler(commands = ["Scroll"])
@@ -129,11 +155,17 @@ def Scroll(message):
 @bot.message_handler(content_types = ["text"])
 def message_check(message):
     global HotkeyMode, ScrollMode
+    TL = Telega.Telega(message)
     text = message.text
     if connection:
         if HotkeyMode:
-            pyautogui.hotkey(text.split("+"))
+            if text != "Show_keyboard_keys":
+                pyautogui.hotkey(text.split("+"))
+            else:
+                bot.send_message(message.chat.id, KEYBOARD_KEYS)
             HotkeyMode = False
+            TL.Reply_btns("Hotkey mode off", True, 4, "/LKM", "/2LKM", "/Enter", "/Bcksp", "/RKM", "/Scroll",
+                          "/Hotkey", "/Shift", "/PrtSc", "/Cam", "/session")
         elif ScrollMode:
             try:
                 steps = int(text)
